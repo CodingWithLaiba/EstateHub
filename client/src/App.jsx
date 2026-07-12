89f98da16b1098ba0e98ddcb5cb82d87cb07b7b2
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-12 22:28:05 +0500

    feat(client): implement routing for Home, About, SignIn, SignUp, and Profile components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SingIn from './pages/SingIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<Home/>}/>
+        <Route path='/about' element ={<About/>}/>
+        <Route path='/sign-in' element ={<SingIn/>}/>
+        <Route path='/sign-up' element ={<SignUp/>}/>
+        <Route path='/profile' element ={<Profile/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }

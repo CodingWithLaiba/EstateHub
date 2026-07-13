e44bfce19dcc226dbcbe617564e950a89e0257e9
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-13 16:02:50 +0500

    feat(api): implement user signup functionality with password hashing

---
+++ api/controllers/authController.js
@@ -0,0 +1,15 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User Created Sucessfully" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};

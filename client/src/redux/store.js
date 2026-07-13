e8023358716210d209cf69d222de3f3365cc9333
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-13 22:12:42 +0500

    feat(store): implement redux-persist for state persistence and update store configuration

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+
+const storage = {
+  getItem: (key) => {
+    return Promise.resolve(localStorage.getItem(key));
+  },
+  setItem: (key, value) => {
+    return Promise.resolve(localStorage.setItem(key, value));
+  },
+  removeItem: (key) => {
+    return Promise.resolve(localStorage.removeItem(key));
+  },
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);

35c423e3da0a33516c2532ff544e2743af91e66a
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-13 16:38:34 +0500

    feat(api): enhance error handling middleware and improve code formatting

--- api/index.js
+++ api/index.js
@@ -1,17 +1,35 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import userRoute from "./routes/userRoute.js"
-import authRoute from "./routes/authRoute.js"
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
+dotenv.config();
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_DB)
-  .then(() => { console.log("mongodb is connected!") })
-  .catch((err) => { console.log(err) })
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("mongodb is connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-  app.use('/api/user',userRoute)
-app.use('/api/auth',authRoute)
-app.listen(process.env.PORT, () => console.log(`Backend Server is running on PORT ${process.env.PORT}`))
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  }),
+
+app.listen(process.env.PORT, () =>
+  console.log(`Backend Server is running on PORT ${process.env.PORT}`),
+);

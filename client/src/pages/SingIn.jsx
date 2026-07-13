76b78910b40abbb111b57bee5329d4d284b79509
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-13 19:09:26 +0500

    feat(ui): implement SignIn component with form handling and error display

--- client/src/pages/SingIn.jsx
+++ client/src/pages/SingIn.jsx
@@ -1,5 +1,74 @@
-export default function SingIn() {
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+
+export default function SignIn() {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password }),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate('/')
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  }; 
+
   return (
-    <div>SingIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-4xl text-center font-bold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          className="border p-3 rounded-lg"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="border p-3 rounded-lg"
+          placeholder="Password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 uppercase text-white p-3 rounded-lg hover:opacity-95"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-1.5 mt-2">
+        <p>Dont Have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-400">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }

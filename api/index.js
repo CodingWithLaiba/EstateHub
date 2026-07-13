c5f1660d398e35c54eccb7a1f8f08557fb7f4297
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-13 16:02:58 +0500

    feat(api): set up user and auth routes in the main application file

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRoute from "./routes/userRoute.js"
+import authRoute from "./routes/authRoute.js"
 dotenv.config()
 
 const app = express()
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_DB)
   .then(() => { console.log("mongodb is connected!") })
   .catch((err) => { console.log(err) })
 
+  app.use('/api/user',userRoute)
+app.use('/api/auth',authRoute)
 app.listen(process.env.PORT, () => console.log(`Backend Server is running on PORT ${process.env.PORT}`))

695f9469131743f350c8bce9d4d863e4ad28c798
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-13 20:33:53 +0500

    feat(ui): integrate Redux for sign-in handling and error management

--- client/src/pages/SingIn.jsx
+++ client/src/pages/SingIn.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice.js";
 
 export default function SignIn() {
-  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
-const navigate = useNavigate()
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -21,19 +26,16 @@ const navigate = useNavigate()
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null)
-      navigate('/')
+      dispatch(signInSuccess(data));
+      navigate("/");
       console.log(data);
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
-  }; 
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">

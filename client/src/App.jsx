005c554dfb3937fab0acc871ee815a7295d956be
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-14 22:55:20 +0500

    feat(routes): add PrivateRoute for profile access control

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SingIn from './pages/SingIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 export default function App() {
   return (
     <div>
@@ -15,7 +16,9 @@ export default function App() {
         <Route path='/about' element ={<About/>}/>
         <Route path='/sign-in' element ={<SingIn/>}/>
         <Route path='/sign-up' element ={<SignUp/>}/>
+        <Route element={<PrivateRoute/>}>
         <Route path='/profile' element ={<Profile/>}/>
+        </Route>
       </Routes>
       </BrowserRouter>
     </div>

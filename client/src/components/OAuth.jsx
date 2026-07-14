19a4fb97c3428751de0f274bf170dc81cb7186c2
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-14 22:32:58 +0500

    feat(ui): add OAuth component for Google sign-in functionality

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,42 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice.js";
+import { app } from "../firebase";
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate= useNavigate()
+  const handleGoogleCheck = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate("/")
+    } catch (error) {
+      console.log("could not sign in with Google:", error);
+    }
+  };
+  return (
+    <button
+      type="button"
+      onClick={handleGoogleCheck}
+      className="bg-red-700 cursor-pointer text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+    >
+      Continue With Google
+    </button>
+  );
+}

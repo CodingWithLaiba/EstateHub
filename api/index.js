40751d29bad1c4c483656b97fa5316a27eee6350
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-15 22:08:01 +0500

    fix(index): correct cookie-parser import and formatting in server setup

--- api/index.js
+++ api/index.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
+import  cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
@@ -20,16 +21,15 @@ mongoose
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal server error";
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
-  }),
-
-app.listen(process.env.PORT, () =>
-  console.log(`Backend Server is running on PORT ${process.env.PORT}`),
-);
+(app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+}),
+  app.listen(process.env.PORT, () =>
+    console.log(`Backend Server is running on PORT ${process.env.PORT}`),
+  ));

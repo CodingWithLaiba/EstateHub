732c6ac734927af683823774bfd29ef1d5ba21fd
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-16 16:52:09 +0500

    feat(UserController): add getUserListing function to retrieve user's listings

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listingModels.js";
 import User from "../models/userModel.js";
 import { errorHandler } from "../utlis/error.js";
 import bcrypt from "bcryptjs";
@@ -45,5 +46,17 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
     console.log(error);
     next(error);
+  }
+};
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id == req.params.id) {
+    try {
+      const listing = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listing);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listing! "));
   }
 };

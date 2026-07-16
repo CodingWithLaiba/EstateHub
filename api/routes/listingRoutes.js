900a88cec4af593a952dd8642ea23290561ade4a
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-16 16:52:16 +0500

    feat(ListingRoutes): add endpoint to retrieve user's listings

--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { createListing } from "../controllers/listingController.js";
+import { getUserListing } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/create", createListing);
+router.get("/get/:id", getUserListing);
 export default router;

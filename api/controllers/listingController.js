d2f455b26642ed0de12c0f17ed7ab634c84d96e7
CodingWithLaiba <laibaijaz904@gmail.com>
2026-07-16 21:54:46 +0500

    fix(ListingRoutes): rename deleteUser to deleteListing and add updateListing endpoint

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -10,7 +10,7 @@ export const createListing = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async (req, res, next) => {
+export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
@@ -21,6 +21,22 @@ export const deleteUser = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     return res.status(200).json("listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can only update your own listing "));
+
+  try {
+   const updateListing = await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    return res.status(200).json(updateListing);
   } catch (error) {
     next(error);
   }
--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createListing,deleteUser } from "../controllers/listingController.js";
+import { createListing,deleteListing,updateListing } from "../controllers/listingController.js";
 import { verifyToken } from "../utlis/verifyUser.js";
 import { getUserListing } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/create", createListing);
 router.get("/get/:id", getUserListing);
-router.delete("/delete/:id",verifyToken,deleteUser)
-
+router.delete("/delete/:id",verifyToken,deleteListing)
+router.post("/update/:id",verifyToken,updateListing)
 export default router;

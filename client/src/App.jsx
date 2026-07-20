0ee79531e262e1d70061c3c5b377242b62820ee0
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-20 22:13:22 +0500

    refactor(App): standardize import statements and improve formatting for readability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SingIn from './pages/SingIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './pages/CreateListing';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingIn from "./pages/SingIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element ={<Home/>}/>
-        <Route path='/about' element ={<About/>}/>
-        <Route path='/sign-in' element ={<SingIn/>}/>
-        <Route path='/sign-up' element ={<SignUp/>}/>
-        <Route element={<PrivateRoute/>}>
-        <Route path='/profile' element ={<Profile/>}/>
-        <Route path='/create-listing' element ={<CreateListing />}/>
-        </Route>
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SingIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }

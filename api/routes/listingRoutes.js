b7fc355dc8e8a0494880021309901f607c341a28
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-20 22:13:10 +0500

    fix(ListingRoutes): add getListing endpoint and comment out unused getUserListing route

--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createListing,deleteListing,updateListing } from "../controllers/listingController.js";
+import { createListing,deleteListing,updateListing,getListing } from "../controllers/listingController.js";
 import { verifyToken } from "../utlis/verifyUser.js";
 import { getUserListing } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/create", createListing);
-router.get("/get/:id", getUserListing);
+// router.get("/get/:id", getUserListing);
 router.delete("/delete/:id",verifyToken,deleteListing)
 router.post("/update/:id",verifyToken,updateListing)
+router.get("/get/:id", getListing);
+
 export default router;

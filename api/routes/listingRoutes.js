852532f3790460ef6a91142643b41251685683c9
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-21 22:11:17 +0500

    feat(Listing): reorganize imports and add route to retrieve all listings

--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { createListing,deleteListing,updateListing,getListing } from "../controllers/listingController.js";
+import {
+  getListings,
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+} from "../controllers/listingController.js";
 import { verifyToken } from "../utlis/verifyUser.js";
 import { getUserListing } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/create", createListing);
-// router.get("/get/:id", getUserListing);
-router.delete("/delete/:id",verifyToken,deleteListing)
-router.post("/update/:id",verifyToken,updateListing)
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;

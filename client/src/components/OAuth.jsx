abb29d9ac863859706d44bf8d0badf7539ca4c85
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-23 14:12:25 +0500

    fix(OAuth): improve code formatting and ensure consistent use of semicolons

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,15 +1,17 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice.js";
 import { app } from "../firebase";
 export default function OAuth() {
   const dispatch = useDispatch();
-  const navigate= useNavigate()
+  const navigate = useNavigate();
   const handleGoogleCheck = async () => {
     try {
       const provider = new GoogleAuthProvider();
+      provider.setCustomParameters({
+        prompt: "select_account",
+      });
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
       const res = await fetch("/api/auth/google", {
@@ -17,6 +19,7 @@ export default function OAuth() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify({
           name: result.user.displayName,
           email: result.user.email,
@@ -25,11 +28,12 @@ export default function OAuth() {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
-      navigate("/")
+      navigate("/");
     } catch (error) {
       console.log("could not sign in with Google:", error);
     }
   };
+
   return (
     <button
       type="button"

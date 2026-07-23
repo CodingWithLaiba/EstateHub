984a1355482038251e0d7ff75806cb1d5898b478
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-23 13:43:47 +0500

    feat(api): add static file serving and update path resolution for client assets

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
 import listingRoute from './routes/listingRoutes.js'
 import  cookieParser from "cookie-parser";
+import path from 'path'
 dotenv.config();
 
 const app = express();
@@ -19,9 +20,17 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve
+
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRoute);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 (app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
